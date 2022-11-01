05fa387708802b1effba3ec50f50fd89c984499e
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-11-01 07:06:21 +0530

    pydict

---
+++ templates/check.py
@@ -0,0 +1,9 @@
+from PyDictionary import PyDictionary
+
+dict = PyDictionary()
+
+print(dict.meaning("commensurate"))
+print(dict.synonym("rejected"))
+
+
+{'Noun': ['the immaterial part of a person; the actuating cause of an individual life', 'a human being', 'deep feeling or emotion', 'the human embodiment of something', 'a secular form of gospel that was a major Black musical genre in the 1960s and 1970s']}
