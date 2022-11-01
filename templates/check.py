8ee095377b0551fb5d981d3599e9e58d3bb61bc9
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-11-01 11:53:09 +0530

    displaying ajax responses

--- templates/check.py
+++ templates/check.py
@@ -7,3 +7,8 @@ print(dict.synonym("rejected"))
 
 
 {'Noun': ['the immaterial part of a person; the actuating cause of an individual life', 'a human being', 'deep feeling or emotion', 'the human embodiment of something', 'a secular form of gospel that was a major Black musical genre in the 1960s and 1970s']}
+
+for(var i=0;i<res.length;i++){
+                console.log("iran");
+                console.log(res[i]);
+            }
